f7a0633117688d839f259014800301b6f8508c1d
haideramjad1 <153447478+haideramjad1@users.noreply.github.com>
2025-01-27 15:12:12 +0500

    compact code

--- WPFSample/obj/Debug/WPFSample_Content.g.i.cs
+++ WPFSample/obj/Debug/WPFSample_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resources/customer_icon.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resources/previous.png")]
 
 
