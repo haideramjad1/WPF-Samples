1214816fce3350c8de5fae8657900776c6bfcf2d
haideramjad1 <153447478+haideramjad1@users.noreply.github.com>
2025-02-12 16:31:22 +0500

    numeric keypad

---
+++ WPFSample/NumericKeyboard1.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+
+namespace WPFSample
+{
+    /// <summary>
+    /// Interaction logic for NumericKeyboard1.xaml
+    /// </summary>
+    public partial class NumericKeyboard1 : Window
+    {
+        public NumericKeyboard1()
+        {
+            InitializeComponent();
+        }
+
+        private void KeyboardButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Close_Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Enter_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SearchTextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Back_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
